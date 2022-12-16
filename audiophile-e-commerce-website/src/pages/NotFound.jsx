1b3c12e1f1e1e74e36939639b2ede406bcb162d0
RudyRivera <combustingkarma@gmail.com>
2022-12-16 14:37:44 -0600

    Not found page completed

--- audiophile-e-commerce-website/src/pages/NotFound.jsx
+++ audiophile-e-commerce-website/src/pages/NotFound.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Categories from '../components/Categories';
+import '../scss/pages/NotFound.css';
 
 export default function NotFound() {
   return (
     <>
-    <main>
-        <div>NotFound Page</div>
+    <main className='not-found'>
+      <div className='not-found__content container'>
+        <img src='https://media.giphy.com/media/JSv2Yg4qUkEDGvYYqY/giphy.gif' alt=''  aria-hidden='true'/>
+        <h2>Who are you?!</h2>
+        <h5>Lets... get you back on track</h5>
+        <a className='subtitle credit' href="https://giphy.com/gifs/cbc-comedy-schittscreek-514-JSv2Yg4qUkEDGvYYqY">via GIPHY</a>
+      </div>
     </main>
+    <Categories />
     </>
   );
 }
--- audiophile-e-commerce-website/src/pages/ProductPage.jsx
+++ audiophile-e-commerce-website/src/pages/ProductPage.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate, Navigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Includes from '../components/Includes';
 import Categories from '../components/Categories';
 import About from '../components/About';
 import RelevantItem from '../components/RelevantItem';
+import NotFound from './NotFound';
 import data from '../js/data.json';
 import '../scss/pages/ProductPage.css';
 
 export default function ProductPage() {
     const [count, setCount] = useState(1);
     const {category, slug} = useParams();
-    const item = data.filter(item => item.category === category && item.slug === slug)[0];
     const navigate = useNavigate();
+    
+    const item = data.filter(item => item.category === category && item.slug === slug)[0];
     const { 
         image,
         name,
@@ -29,7 +31,6 @@ export default function ProductPage() {
     const featureParagraphs = features.split('\n\n').map(para => <p key={uuidv4()}>{para}</p>);
     const includedComps = inc.map(info => <Includes key={uuidv4()} info={info}/>);
     const othersComps = others.map(item => <RelevantItem key={uuidv4()} item={item}/>);
-    console.log(others)
 
     const addCount = () => {
         setCount(prevCount => prevCount + 1);
@@ -39,110 +40,112 @@ export default function ProductPage() {
     };
 
     return (
+        <>
         <div className='container'>
-        <button className='btn-link btn-link__back btn-link--txt' onClick={()=>navigate(-1)}>go back</button>
-        <main className='product'>
-            <div className='product__overview'>
-                <picture aria-hidden='true'>
-                    <source
-                        media='(min-width: 1100px)'
-                        srcSet={`.${desktop}`}
-                    />
-                    <source
-                        media='(min-width: 700px)'
-                        srcSet={`.${tablet}`}
-                    />
-                    <source
-                        media='(min-width: 375px)'
-                        srcSet={`.${mobile}`}
-                    />
-                    <img src={`.${mobile}`} className='product__overview__img'/>
-                </picture>
-                <div className='product__overview__info'>
-                    {isNew ? <span className='overline accent uppercase tag'>new product</span> : ''}
-                    <h2>{name}</h2>
-                    <p>{desc}</p>
-                    <span className='price'>$ {Intl.NumberFormat('en-US').format(price)}</span>
-                    <div className='controls'>
-                        <div className='btn--txt'>
-                            <button className='control-btn' onClick={subCount} disabled={count === 1 ? true : false}>-</button>
-                            <span className='count'>{count}</span>
-                            <button className='control-btn' onClick={addCount}>+</button>
+            <button className='btn-link btn-link__back btn-link--txt' onClick={()=>navigate(-1)}>go back</button>
+            <main className='product'>
+                <div className='product__overview'>
+                    <picture aria-hidden='true'>
+                        <source
+                            media='(min-width: 1100px)'
+                            srcSet={`.${desktop}`}
+                        />
+                        <source
+                            media='(min-width: 700px)'
+                            srcSet={`.${tablet}`}
+                        />
+                        <source
+                            media='(min-width: 375px)'
+                            srcSet={`.${mobile}`}
+                        />
+                        <img src={`.${mobile}`} className='product__overview__img'/>
+                    </picture>
+                    <div className='product__overview__info'>
+                        {isNew ? <span className='overline accent uppercase tag'>new product</span> : ''}
+                        <h2>{name}</h2>
+                        <p>{desc}</p>
+                        <span className='price'>$ {Intl.NumberFormat('en-US').format(price)}</span>
+                        <div className='controls'>
+                            <div className='btn--txt'>
+                                <button className='control-btn' onClick={subCount} disabled={count === 1 ? true : false}>-</button>
+                                <span className='count'>{count}</span>
+                                <button className='control-btn' onClick={addCount}>+</button>
+                            </div>
+                            <button className='link-btn btn--txt link-btn--orange'>add to cart</button>
                         </div>
-                        <button className='link-btn btn--txt link-btn--orange'>add to cart</button>
                     </div>
                 </div>
-            </div>
-            <div className='product__about'>
-                <div className='product__about__features'>
-                    <h3 className='uppercase'>features</h3>
-                    {featureParagraphs}
-                </div>
-                <div className='product__about__included'>
-                    <h3 className='uppercase'>in the box</h3>
-                    <div>
-                        {includedComps}
+                <div className='product__about'>
+                    <div className='product__about__features'>
+                        <h3 className='uppercase'>features</h3>
+                        {featureParagraphs}
+                    </div>
+                    <div className='product__about__included'>
+                        <h3 className='uppercase'>in the box</h3>
+                        <div>
+                            {includedComps}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='product__imgs' aria-hidden='true'>
-                <div className='product__imgs__row-one'>
-                <picture aria-hidden='true'>
-                    <source
-                        media='(min-width: 1100px)'
-                        srcSet={`.${first.desktop}`}
-                    />
-                    <source
-                        media='(min-width: 700px)'
-                        srcSet={`.${first.tablet}`}
-                    />
-                    <source
-                        media='(min-width: 375px)'
-                        srcSet={`.${first.mobile}`}
-                    />
-                    <img src={`.${first.mobile}`}/>
-                </picture>
-                <picture aria-hidden='true'>
-                    <source
-                        media='(min-width: 1100px)'
-                        srcSet={`.${second.desktop}`}
-                    />
-                    <source
-                        media='(min-width: 700px)'
-                        srcSet={`.${second.tablet}`}
-                    />
-                    <source
-                        media='(min-width: 375px)'
-                        srcSet={`.${second.mobile}`}
-                    />
-                    <img src={`.${second.mobile}`}/>
-                </picture>
+                <div className='product__imgs' aria-hidden='true'>
+                    <div className='product__imgs__row-one'>
+                    <picture aria-hidden='true'>
+                        <source
+                            media='(min-width: 1100px)'
+                            srcSet={`.${first.desktop}`}
+                        />
+                        <source
+                            media='(min-width: 700px)'
+                            srcSet={`.${first.tablet}`}
+                        />
+                        <source
+                            media='(min-width: 375px)'
+                            srcSet={`.${first.mobile}`}
+                        />
+                        <img src={`.${first.mobile}`}/>
+                    </picture>
+                    <picture aria-hidden='true'>
+                        <source
+                            media='(min-width: 1100px)'
+                            srcSet={`.${second.desktop}`}
+                        />
+                        <source
+                            media='(min-width: 700px)'
+                            srcSet={`.${second.tablet}`}
+                        />
+                        <source
+                            media='(min-width: 375px)'
+                            srcSet={`.${second.mobile}`}
+                        />
+                        <img src={`.${second.mobile}`}/>
+                    </picture>
+                    </div>
+                    <picture aria-hidden='true'>
+                        <source
+                            media='(min-width: 1100px)'
+                            srcSet={`.${third.desktop}`}
+                        />
+                        <source
+                            media='(min-width: 700px)'
+                            srcSet={`.${third.tablet}`}
+                        />
+                        <source
+                            media='(min-width: 375px)'
+                            srcSet={`.${third.mobile}`}
+                        />
+                        <img src={`.${second.mobile}`} className='third'/>
+                    </picture>
                 </div>
-                <picture aria-hidden='true'>
-                    <source
-                        media='(min-width: 1100px)'
-                        srcSet={`.${third.desktop}`}
-                    />
-                    <source
-                        media='(min-width: 700px)'
-                        srcSet={`.${third.tablet}`}
-                    />
-                    <source
-                        media='(min-width: 375px)'
-                        srcSet={`.${third.mobile}`}
-                    />
-                    <img src={`.${second.mobile}`} className='third'/>
-                </picture>
-            </div>
-            <div className='product__relevant'>
-                <h3 className='uppercase'>you may also like</h3>
-                <div className='product__relevant-items'>
-                    {othersComps}
+                <div className='product__relevant'>
+                    <h3 className='uppercase'>you may also like</h3>
+                    <div className='product__relevant-items'>
+                        {othersComps}
+                    </div>
                 </div>
-            </div>
-        </main>
-        <Categories />
-        <About />
+            </main>
+            <Categories />
+            <About />
         </div>
+        </>
     );
 }
